c10cd77cbb3dd434308e0a8193933c7d002d897c
rschuessler <rschuessler@csc.com>
2017-12-11 19:50:42 +0100

    animalDetails Hide and Show

--- src/pages/animal-details/animal-details.ts
+++ src/pages/animal-details/animal-details.ts
@@ -14,27 +14,77 @@ export class AnimalDetailsPage {
   animal: any;
   saveArray =[];
   user = [];
+  breed: string;
+  showPos;
+  hidePos;
+  showU;
+  hideU;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public listService: AnimalListService) {
     this.animal = this.navParams.get("animal");
+    this.showPos= false;
+    this.hidePos= true;
+    this.showU= false;
+    this.hideU= true;
   }
 
   ionViewDidLoad() {
-    this.showUser();
+    this.fillUserArray();
+    this.showBreed();
   }
 
-  showUser(){
+  /**
+   * Holt sich die User Details aus der Datenbank
+   */
+  fillUserArray(){
     let uId = this.animal.uId;
     this.listService.getUserListRef().orderByChild("uId").equalTo(uId).on("child_added", snapshot => {  
       this.saveArray.push(snapshot);
-      console.log(snapshot.val());
       this.user = this.saveArray.slice().reverse().map( c => ({
         key: c.key, ... c.val()
       }));
     });
   }
 
+  /**
+   * Öffnet die UserDetailsPage und übergibt einen user
+   * @param user 
+   */
   showUserDetails(user){
     this.navCtrl.push(UserDetailsPage, {user: user});
+  }
+
+  /**
+   * Zeigt die Rassen mit Leerzeichen und Komma getrennt
+   */
+  showBreed(){
+    this.breed = this.animal.breed.toString();
+    this.breed.replace(",", ", ");
+    console.log(this.breed.replace(",", ", "));
+  }
+
+  /**
+   * Zeigt Positions Details an
+   */
+  showPosition(){
+    this.hidePos = false;
+    this.showPos = true;
+  }
+
+  /**
+   * Verbirgt Pos Details
+   */
+  hidePosition(){
+    this.hidePos = true;
+    this.showPos = false;
+  }
+
+  showUser(){
+    this.hideU = false;
+    this.showU = true;
+  }
+  hideUser(){
+    this.hideU = true;
+    this.showU = false;
   }
 }
